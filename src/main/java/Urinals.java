51f78b6c0294a9210e6e4e964f733f50bdb940b7
arajpur2 <arajpur2@asu.edu>
2022-10-27 23:32:42 -0700

    test case to check if any letters exist in the input String successfully passed

--- src/main/java/Urinals.java
+++ src/main/java/Urinals.java
@@ -59,7 +59,7 @@ public class Urinals {
                 try {
                     System.out.println("PLEASE ENTER THE URINAL REPRESENTATION STRING");
                     Scanner sc2 = new Scanner(System.in);
-                    inputString[0] = sc2.nextLong();
+                    inputString[0] = sc2.nextLine();
                 } catch (Exception e){
                     System.out.println("Wrong type of input!");
                 }
